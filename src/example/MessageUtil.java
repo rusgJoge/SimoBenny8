f007dd2c645ede1b50865ee0a9453ec1fa35903c
rusgJoge <zhangtan55797@163.com>
2021-03-31 18:57:50 +0800

    messageunit modificata

--- src/example/MessageUtil.java
+++ src/example/MessageUtil.java
@@ -1,9 +1,11 @@
 package example;
 
+import java.util.logging.Logger;
+
 public class MessageUtil {
 	
 	private String message;
-
+	private static final Logger logger1 = Logger.getLogger(MessageUtil.class.getName());
 	   //Constructor
 	   //@param message to be printed
 	   public MessageUtil(String message){
@@ -12,7 +14,7 @@ public class MessageUtil {
 	      
 	   // prints the message
 	   public String printMessage(){
-	      System.out.println(message);
+	      logger1.info(message);
 	      return message;
 	   }   
 
