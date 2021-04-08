c9235159fcf03ce9220ba38ca90c59614076ac6b
rusgJoge <zhangtan55797@163.com>
2021-04-09 07:24:51 +0800

    analyzing project

--- src/example/IdCommit.java
+++ src/example/IdCommit.java
@@ -1,6 +1,8 @@
 package example;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -9,20 +11,62 @@ import java.util.logging.Logger;
 public class IdCommit {
 	
 	private static final Logger logger = Logger.getLogger(IdCommit.class.getName());
+	
+	public static void commitString(String wordToSearch, String project,String fileName, int i) throws IOException {
+		
+		String s;
+        Process p;
+        String keyword = "commit";
+        String outname = fileName + "TotalCommit.txt";
+        FileWriter result = null;
+        //String project = "C:\\Users\\Simone Benedetti\\Documents\\Programmazione JAVA\\Bahir";
+        //String wordToSearch = "BAHIR-49";
+        
+        try {
+            p = Runtime.getRuntime().exec("cmd /c cd "+project+"&& git log --grep="+wordToSearch+" --date=iso-strict --name-status --stat HEAD --abbrev-commit");
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            if(i != 0) {
+            	result = new FileWriter(outname,true);
+            }else {
+            	result = new FileWriter(outname);
+            }
+            while ((s = br.readLine()) != null) {
+            	result.write(s);
+            	result.append("\n");
+            }	
+            p.waitFor();
+            p.destroy();
+            
+        }catch (Exception e) {
+        	logger.log(Level.WARNING,"Exception found");
+        }finally {
+            try {
+	               result.flush();
+	               result.close();
+	               //System.out.println("file chiuso");
+	            } catch (IOException e) {
+	               System.out.println("Error while flushing/closing fileWriter !!!");
+	               e.printStackTrace();
+	            }
+        }
+        return;
+        
+    }
+		
+	
 
 	public static void main(String[] args) {
 		String s;
         Process p;
         String keyword = "commit";
-        String project = "C:\\Users\\Simone Benedetti\\Documents\\Programmazione JAVA\\AddedIDProject";
-        String wordToSearch = "added";
+        String project = "C:\\Users\\Simone Benedetti\\Documents\\Programmazione JAVA\\Bahir";
+        String wordToSearch = "BAHIR-49";
         
         try {
             p = Runtime.getRuntime().exec("cmd /c cd "+project+"&& git log --grep="+wordToSearch+" --date=iso-strict --name-status --stat HEAD --abbrev-commit");
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
             while ((s = br.readLine()) != null) {
             	if (s.toLowerCase().indexOf(keyword.toLowerCase()) != -1 ) {
-            		logger.info("Keyword matched the string");
             		String[] words = s.split(" ");
             		String id = words[1];
             		logger.log(Level.INFO, "Commit id: {0}", id);
---
+++ src/example/ProcessControlChart.java
@@ -0,0 +1,46 @@
+package example;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.JSONException;
+
+public class ProcessControlChart {
+	
+	private IdCommit ic;
+	private static ArrayList<String> id;
+	
+	public static void main(String[] args) {
+		
+		String progName = "BAHIR";
+		String project1 = "C:\\Users\\Simone Benedetti\\Documents\\Programmazione JAVA\\Bahir";
+		String project2 = "C:\\Users\\Simone Benedetti\\Documents\\Programmazione JAVA\\Bahir-website";
+		String project3 = "C:\\Users\\Simone Benedetti\\Documents\\Programmazione JAVA\\Flink";
+		
+		try {
+			id = RetrieveTicketsID.getIdCommit(progName);
+		} catch (JSONException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		for(int i=0; i<id.size(); i++) {
+			try {
+				IdCommit.commitString(id.get(i), project1, "bahir" , i);
+				IdCommit.commitString(id.get(i), project2,"bahir-website", i);
+				IdCommit.commitString(id.get(i), project3, "flink",i);
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+
+}
